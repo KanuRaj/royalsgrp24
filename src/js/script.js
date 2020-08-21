0a78462963729c1a0803f6ff798e44148f88dd26
KanuRaj <rajputkaran410@gmail.com>
2020-08-22 00:55:12 +0530

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+goToTop = document.getElementById("goToTop");
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        goToTop.style.display = "block";
+    } else {
+        goToTop.style.display = "none";
+    }
+}
+
+
+goToTop.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
