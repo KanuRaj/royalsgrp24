89981a2e1ac5eec15204b32058f71fdbd1969a02
KanuRaj <63672182+KanuRaj@users.noreply.github.com>
2020-08-22 22:56:43 +0530

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,8 @@ goToTop.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 });
+
+document.querySelector("#goToTop i").addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
