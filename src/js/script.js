68b9f1a1a0d4f5a7e590b877bef3f0ea47257b8b
KanuRaj <63672182+KanuRaj@users.noreply.github.com>
2020-08-29 02:06:35 +0530

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,43 @@ $('#goToTop').click(function () {
     }, 0, 'easeInOutExpo');
     return false;
 });
+// transform: translate3d(0px, -1px, 0px)
+$(document).on("mouseover", ".sr-card", function () {
+    $(this).addClass('shadow')
+    $(this).css({
+        '-webkit-transform': 'translate3d(0px, -5px, 0px)',
+        '-moz-transform': 'translate3d(0px, -5px, 0px)',
+        '-ms-transform': 'translate3d(0px, -5px, 0px)',
+        '-o-transform': 'translate3d(0px, -5px, 0px)',
+        'transform': 'translate3d(0px, -5px, 0px)'
+    });
+});
+$(document).on("mouseout", ".sr-card", function () {
+    $(this).removeClass('shadow')
+    $(this).css({
+        '-webkit-transform': 'translate3d(0px, 0px, 0px)',
+        '-moz-transform': 'translate3d(0px, 0px, 0px)',
+        '-ms-transform': 'translate3d(0px, 0px, 0px)',
+        '-o-transform': 'translate3d(0px, 0px, 0px)',
+        'transform': 'translate3d(0px, 0px, 0px)'
+    });
+});
+
+function setProductDetailsHeight(params) {
+    var winwid = window.innerWidth;
+    if (winwid >= 992) {
+        var productImgHeight = $(".product-img").height() + "px";
+        $('.product-details').css({ 'max-height': productImgHeight });
+    } else {
+        $('.product-details').css({ 'max-height': 'initial !important' });
+    }
+}
+setProductDetailsHeight();
+$(window).on("resize", () => {
+    setProductDetailsHeight();
+});
+
+$(document).on("click", ".addToFav", function (e) {
+    // $(this).children('i').removeClass("far");
+    $(this).children('i').toggleClass("fas");
+});
